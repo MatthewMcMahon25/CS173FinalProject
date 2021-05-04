10288dbb5b54b8f286eec24c8a5944ef544b381d
macma <macma@DESKTOP-4I3DL6H.ursinus.local>
2021-05-04 14:52:18 -0400

    initial commit

---
+++ src/csfinalproject/food.java
@@ -0,0 +1,22 @@
+package csfinalproject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author matt and emily
+ */
+public class food {
+    private String foodName;
+    private int calories;
+    private int protein;
+    private int fat;
+    private int carbs;
+    private double cost;
+}
+
+
