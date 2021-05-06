8ffcc0dc715ba7d258cab2ec532b6260d495c992
macma <macma@DESKTOP-4I3DL6H.ursinus.local>
2021-05-06 12:13:56 -0400

    for loop that seperates csv data into arrays

--- src/csfinalproject/CSFinalProject.java
+++ src/csfinalproject/CSFinalProject.java
@@ -8,6 +8,7 @@ package csfinalproject;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -17,14 +18,14 @@ import java.util.Scanner;
  */
 public class CSFinalProject {
     /**
-     * Returns a String with the text from a selected file
+     * read a file's contents given a filename, and return a String with the data
      * @param path
-     * @return String result
+     * @return a String with all the file data
      */
-    public static String readFileString(String path){
+    public static String readFileString(String fileName){
         String result = "";
         try {
-            result = Files.readString(Paths.get(path));
+            result = Files.readString(Paths.get(fileName));
         }catch(IOException e) {
             System.err.println(e);
         }
@@ -34,23 +35,40 @@ public class CSFinalProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String itemsData = readFileString("items.txt");
+        System.out.println(itemsData);
         
-        //Split each line, based on ""
+        //Split each line into an item w/ contents into an array
+        String[] lines = itemsData.split("\n");        
         
+        //HashMap<String, HashMap<String, >> places = new HashMap<String, Hashmap<>>();
         
         //Split each line into individual parts based on ","
-        
+        for(String line: lines) {
+            System.out.println("Here's a line: " + line);
+            
+            String[] values = line.split(",");
+            
+            System.out.println(Arrays.toString(values));
+            
+            String foodName = values[0];
+            int calories = Integer.parseInt(values[1]);
+            int protein = Integer.parseInt(values[2]);
+            int fat = Integer.parseInt(values[3]);
+            int carbs = Integer.parseInt(values[4]);
+            double cost = Double.parseDouble(values[5]);
+                  
+            Food f = new Food(foodName, calories, protein, fat, carbs, cost);
+            
+        }
         //for loop
         //Based upon position in array, set that to a food array, possibly hashmap
         //where you have [foodName, calories, protein, fats, carbs, cost]
         
         
         
-        food food1 = new food("Gatorade Bar", 330, 20, 10, 43, 4.30);
-        
         System.out.println("Welcome to the online C-Store! What would you like to purchase: ");
-        String fileContents = readFileString("items.txt");
-        System.out.println(fileContents);
+        
         
     }  
 }
